d7b0dcdc05958a5edeeaf6e98021656075fbb357
vineethkamisetty <kamisetty.vineeth@gmail.com>
2017-04-20 09:59:21 -0400

    Upadated code to test different network model

--- CNN.py
+++ CNN.py
@@ -1,30 +1,36 @@
 import sys
+
+import tensorflow
 import tflearn
-from tflearn.layers.core import input_data, dropout, fully_connected
-from tflearn.layers.conv import conv_2d, max_pool_2d
-from tflearn.layers.estimator import regression
-from tflearn.layers.normalization import local_response_normalization
+from tflearn import *
 from util import getData
 
-EMOTIONS = ['angry', 'disgusted', 'fearful', 'happy', 'sad', 'surprised', 'neutral']
+EMOTIONS = ['angry', 'fearful', 'happy', 'sad', 'surprised', 'neutral']
 
 
 def network():
     network = input_data(shape=[None, 48, 48, 1])
+
     network = conv_2d(network, 64, 5, activation='relu')
+    network = max_pool_2d(network, 2, strides=2)
+    network = dropout(network, 0.5)
     network = local_response_normalization(network)
-    network = max_pool_2d(network, 3, strides=2)
     network = conv_2d(network, 64, 5, activation='relu')
-    network = max_pool_2d(network, 3, strides=2)
-    network = conv_2d(network, 128, 4, activation='relu')
-    network = dropout(network, 0.3)
-    network = fully_connected(network, 3072, activation='relu')
+    network = max_pool_2d(network, 2, strides=2)
+    network = dropout(network, 0.5)
+    network = local_response_normalization(network)
+    network = conv_2d(network, 128, 5, activation='relu')
+    network = dropout(network, 0.5)
+    network = fully_connected(network, 1024, activation='relu')
+    network = dropout(network, 0.5)
+    network = fully_connected(network, 1024, activation='relu')
+    network = dropout(network, 0.5)
     network = fully_connected(network, len(EMOTIONS), activation='softmax')
 
     network = regression(network,
                          optimizer='adam',
                          loss='categorical_crossentropy',
-                         learning_rate=0.001)
+                         learning_rate=0.001)  # need to check with different learning rate
 
     model = tflearn.DNN(network, tensorboard_verbose=3)
     return model
@@ -34,35 +40,41 @@ def getrawnetwork():
     return network()
 
 
-def getsavednetwork(savepath='./SavedModels/model.tfl'):
+def getsavednetwork(savepath='./SavedModels/model_ab_d.tfl'):
     model = getrawnetwork()
     model.load(savepath)
     return model
 
 
-def train():
-    X_train, Y_train, X_valid, Y_valid, X_test, Y_test = getData()
+def train(cont=False):
+    X_train, Y_train, X_valid, Y_valid, X_test, Y_test = getData()  # need to train on different data sets
     X_train = X_train.transpose((0, 2, 3, 1))
     X_valid = X_valid.transpose((0, 2, 3, 1))
 
-    model = getrawnetwork()
+    tensorflow.reset_default_graph()  # to reset the model graph. problem with loading the weights
+    if cont:
+        model = getsavednetwork()  # need to change back to getrawnetwork()
+    else:
+        model = getrawnetwork()
     model.fit(
         X_train, Y_train,
         validation_set=(X_valid, Y_valid),
-        n_epoch=1,
-        batch_size=50,
+        n_epoch=20,
+        batch_size=100,
         shuffle=True,
         show_metric=True,
         snapshot_step=200,
         snapshot_epoch=True,
-        run_id='emotion_recognition'
+        run_id='emotion_recognition_ab_d'
     )
-    model.save('./SavedModels/model.tfl')
+    model.save('./SavedModels/model_ab_d.tfl')
 
 
 def test():
-    _, _, _, _, X_test, Y_test = getData()
+    _, _, _, _, X_test, Y_test = getData()  # need to test on different datasets
     X_test = X_test.transpose((0, 2, 3, 1))
+
+    tensorflow.reset_default_graph()
 
     model = getsavednetwork()
     score = model.evaluate(X_test, Y_test, batch_size=50)
@@ -70,6 +82,8 @@ def test():
 
 
 def predit(X):
+    tensorflow.reset_default_graph()
+
     model = getsavednetwork()
     X = X.transpose((0, 2, 3, 1))
     pred = model.predict(X)
@@ -82,6 +96,9 @@ if __name__ == "__main__":
         train()
         test()
     elif sys.argv[1] == 'train':
-        train()
+        if len(sys.argv) == 3 and sys.argv[2] == 'continue':
+            train(cont=True)
+        else:
+            train()
     elif sys.argv[1] == 'test':
         test()
